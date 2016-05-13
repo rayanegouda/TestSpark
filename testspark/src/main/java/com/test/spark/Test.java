b3aaf003eae0089632ed4c57a7b266e6c621b3c1
rayanegouda <rayanegouda@gmail.com>
2016-05-13 16:25:54 +0200

    jbjb

--- testspark/src/main/java/com/test/spark/Test.java
+++ testspark/src/main/java/com/test/spark/Test.java
@@ -4,7 +4,17 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("cool");
+		JavaRDD<String> textFile = sc.textFile("hdfs://...");
+		JavaRDD<String> words = textFile.flatMap(new FlatMapFunction<String, String>() {
+		  public Iterable<String> call(String s) { return Arrays.asList(s.split(" ")); }
+		});
+		JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+		  public Tuple2<String, Integer> call(String s) { return new Tuple2<String, Integer>(s, 1); }
+		});
+		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+		  public Integer call(Integer a, Integer b) { return a + b; }
+		});
+		counts.saveAsTextFile("hdfs://...");
 	}
 
 }
